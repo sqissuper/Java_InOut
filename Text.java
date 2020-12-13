bc4826ed3ded1eb64e6128e5cdb00283d62d9088
sqissuper <1764491917@qq.com>
2020-12-13 13:23:01 +0800

    最常见输入输出

---
+++ Text.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Text {
+    public static void main(String[] args){
+        //数组输入
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        //一维数组
+//        int[] arr = new int[3];
+//        int i = 0;
+//
+//        //循环输入输出
+//        for(i = 0; i < 3; i++)
+//        {
+//            arr[i] = sc.nextInt();
+//            System.out.println(arr[i]);
+//        }
+//        //这也是打印数组的一种
+//        for(int x: arr){
+//            System.out.println(x);
+//        }
+//
+//        //第三种输出
+//        System.out.println(Arrays.toString(arr));
+
+
+
+
+
+        //输入
+//        Scanner sc = new Scanner(System.in);
+////
+////        //第一种写法
+//        int a = sc.nextInt();
+//        byte b = sc.nextByte();
+//        short c = sc.nextShort();
+//        long d = sc.nextLong();
+//        double e = sc.nextDouble();
+//        float f = sc.nextFloat();
+//        String g = sc.next();
+//        String name = sc.next();
+//        String name1 = sc.nextLine();
+
+
+        //第二种写法
+//        Scanner scanner = new Scanner(System.in);
+//        int a1;
+//        byte b1;
+//        short c1;
+//        long d1;
+//        double e1;
+//        float f1;
+//        a1 = scanner.nextInt();
+//        b1 = scanner.nextByte();
+//        c1 = scanner.nextShort();
+//        d1 = scanner.nextLong();
+//        e1 = scanner.nextDouble();
+//        f1 = scanner.nextFloat();
+
+
+//        //不换行输出 d e f 输出加了空格
+//        System.out.print(a);
+//        System.out.print(b);
+//        System.out.print(c);
+//        System.out.print(d + " ");
+//        System.out.print(e + " ");
+//        System.out.print(f + " ");
+//        System.out.println(g);
+//        System.out.print(name);
+//        System.out.print(name1);
+
+
+        //换行输出
+//        System.out.println(a1);
+//        System.out.println(b1);
+//        System.out.println(c1);
+//        System.out.println(d1);
+//        System.out.println(e1);
+//        System.out.println(f1);
+//        System.out.println(g);
+//        System.out.println(name);
+//        System.out.println(name1);
+    }
+}
